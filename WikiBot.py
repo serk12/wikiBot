8c9170e1a864f129678739c25363a3a87ac32035
serk <marcprat_maso@hotmail.com>
2016-03-28 12:53:36 +0200

    get token from outside git folder

--- WikiBot.py
+++ WikiBot.py
@@ -3,18 +3,18 @@ import wikipedia
 from telebot import util
 
 
-API_TOKEN = input("get token: ")
+API_TOKEN = open("../token", "rb").read()
 
 bot = telebot.TeleBot(API_TOKEN)
 
-id_leng = {}
+id_lang = {}
 
 def loggin(id):
-    if not id_leng.get(id):
-        id_leng[id] = "en"
+    if not id_lang.get(id):
+        id_lang[id] = "en"
 	return "en"
     else:
-	return id_leng[id]
+	return id_lang[id]
 
 def set_lang(id):
     wikipedia.set_lang(loggin(id))
@@ -26,7 +26,7 @@ explein = "this bot search in wikipedia www.wikipedia.org \nyou can ask me by se
 
 commands = {'help' : "ask for help",
            'info' : "get the summary of a argument",
-           'lenguage' : "set the lenguage of the search",
+           'language' : "set the language of the search",
            'try' : "gives a suggest of a posible page you are looking for",}
 
 iso_639_1 = "aa ab ace ady af ak als am an ang ar arc arz as ast av ay az azb ba bar bat-smg bcl be be-tarask bg bh bi bjn bm bn bo bpy br bs bug bxr ca cbk-zam cdo ce ceb ch cho chr chy ckb co cr crh cs csb cu cv cy da de diq dsb dv dz ee el eml en eo es et eu  ext fa ff fi fiu-vro fj fo fr frp frr fur fy ga gag gan gd gl glk gn gom got gu gv ha hak haw he hi hif ho hr hsb ht hu hy hz ia id ie ig ii ik ilo io is it iu ja jbo jv ka kaa kab kbd kg ki kj kk kl km kn ko koi kr krc ks ksh ku kv kwy la lad lb lbe lez lg li lij lmo ln lo lrc lt ltg lv mai map-bms mdf mg mh mhr mi min mk ml mn mr mrj ms mt mus mwl my myv mzn na na nap nds nds-nl ne new ng nl nn no nov nrm nso nv ny oc om or os pa pag pam pap pcd pdc pfl pi pih pl pms pnb pnt ps pt qu rm rmy rn ro roa-rup roa-tara ru rue rw sa sah sc scn sco sd se sg sh si simple sk sl sm sn so sq sr srn ss st stq su sv sw szl ta te tet tg th ti tk tl tn to tpi tr ts tt tum tw ty tyv udm ug uk ur uz ve vec vep vi vls vo wa war wo wuu xal xh xmf yi yo za zea zh zh-classical zh-min-nan zh-yue zu"
@@ -56,14 +56,14 @@ def smallInfo(mes):
     for text in spliter_summary:
         bot.send_message(mes.chat.id, text)
     
-@bot.message_handler(commands = ['lenguage'])
-def lenguage(mes):
-    get = del_comand('lenguage', mes)
+@bot.message_handler(commands = ['language'])
+def language(mes):
+    get = del_comand('language', mes)
     if (iso_639_1.find(get) != -1):
-        id_leng[mes.chat.id] = get
-        bot.send_message(mes.chat.id, 'lenguage set to: ' + get)
+        id_lang[mes.chat.id] = get
+        bot.send_message(mes.chat.id, 'language set to: ' + get)
     else:
-        bot.send_message(mes.chat.id, "this lenguage don't exist")
+        bot.send_message(mes.chat.id, "this language don't exist in ISO 639-1")
 
 @bot.message_handler(commands = ['try'])
 def try_search(mes):
